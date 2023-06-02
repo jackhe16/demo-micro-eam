1d0d0793a0059580d8ad61d0ca900f513e377ec7
jackhe16 <jackhe16@126.com>
2023-06-02 14:46:38 +0800

    chore: demo

---
+++ src/my-page.js
@@ -0,0 +1,6 @@
+// my-page.js
+export function MyPage() {
+  // window.rawDocument.querySelector('link[rel="icon"]').setAttribute('href', 'https://ahooks.js.org/simple-logo.svg');
+
+  return <div>eeam</div>;
+}
